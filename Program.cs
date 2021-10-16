946b1bad6b1537c777d1e16cb8ea7b0c455c587d
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-10-16 23:54:30 +0530

    As a User Need to Follow Pre-Defined Password Rules .Rule-1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace User_Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          UC5 Passwordchar = new UC5();
+            // Console.WriteLine("Enter Country code in start  eg(91) for India");
+            Console.WriteLine("Enter the Password");
+
+            name5 = Console.ReadLine();
+
+            result5 = Passwordchar.PasswordCheck(name5);
+            Console.WriteLine(result5);
+
+            Console.WriteLine("******************");
+}
+}
+}
